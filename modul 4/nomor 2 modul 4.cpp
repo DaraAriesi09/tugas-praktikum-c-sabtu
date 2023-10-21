78ee7928797593d6bc1c2895d4d33c54f8c12020
unknown <daraariesi1@gmail.com>
2023-10-21 15:20:25 +0700

    first commit

--- modul 4/nomor 2 modul 4.cpp
+++ modul 4/nomor 2 modul 4.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
-
-int main ()
+#include <cmath>
+#include <math.h>
+using namespace std;
+int main()
 {
-	char nim[] = "2341055";
-	char nama[] = "Dara Ariesi Zainal";
-	char jurusan[] = "Teknik Informatika";
+	int bilangan;
 	
-	printf("nim = %s\n", nim);
-	printf("nama = %s\n", nama);
-	printf("jurusan = %s\n", jurusan);
+	cout<<"Masukan sebuah bilangan: ";
+	cin>>bilangan;
 	
+	if(bilangan>50)
+	    bilangan=bilangan+10;
+	else
+	bilangan=bilangan-25;
+	cout<<"Bilangan menjadi : " <<bilangan;
+	
+	return 0;
 }
 	
